b3746440983dad6bcb8da3e0ead0b716f0a11830
Hugo Pedro <41404442+hugopedro@users.noreply.github.com>
2018-11-27 19:17:20 -0200

    Dependente handler

---
+++ src/Handler/Dependente.hs
@@ -0,0 +1,17 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE QuasiQuotes #-}
+module Handler.Dependente where
+
+import Import
+import Text.Lucius
+import Text.Julius
+import Database.Persist.Sql
+getDependenteR :: Handler Html
+getDependenteR =  undefined
+        
+        
+        
