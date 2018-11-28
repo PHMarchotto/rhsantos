ed607d6a9ab82c6388dd8a4b384362f3ca997836
Paulo Henrique Marchotto <phomarchotto@hotmail.com>
2018-11-28 00:20:13 +0000

    HomePage

--- src/Application.hs
+++ src/Application.hs
@@ -42,6 +42,7 @@ import Handler.Funcionario
 import Handler.Dependente
 import Handler.Projeto
 import Handler.Usuario
+import Handler.Home
 
 -- This line actually creates our YesodDispatch instance. It is the second half
 -- of the call to mkYesodData which occurs in Foundation.hs. Please see the
---
+++ src/Handler/Home.hs
@@ -0,0 +1,17 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE QuasiQuotes #-}
+module Handler.Home where
+
+import Import
+import Text.Lucius
+import Text.Julius
+import Database.Persist.Sql
+
+getHomeR :: Handler Html
+getHomeR = do
+    defaultLayout $ do 
+        $(whamletFile "templates/home.hamlet")
