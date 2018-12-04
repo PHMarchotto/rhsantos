83b10cf952d233c3d4b3f47edaddc7ecafb0a06c
Levi Moraes <levimoraes100@gmail.com>
2018-12-04 20:36:00 +0000

    Handler Departamento

--- src/Handler/Departamento.hs
+++ src/Handler/Departamento.hs
@@ -10,3 +10,29 @@ import Import
 import Text.Lucius
 import Text.Julius
 import Database.Persist.Sql
+
+formDepartamento :: FormInput Handler(Int,Text,Text)
+formDepartamento = pure (,)
+    <*> ireq intField "id"
+    <*> ireq textField "nome"
+    <*> ireq textField "local"
+
+getDepartamentosR :: Handler Html   
+getDepartamentosR = do 
+    departamentos <- runDB $ selectList [] [Asc DepartamentoId]
+    defaultLayout $ do 
+        $(whamletFile "templates/departamentos.hamlet")
+        
+postSalvarDepartamentoR :: Handler Html
+postSalvarDepartamentoR = do 
+    ((res,_),_) <- runFormPost formDepartamento
+    case res of 
+        FormSuccess departamento -> do 
+            deptoid <- runDB $ insert departamento
+            redirect DepartamentosR
+        _ -> redirect MenuR
+
+postApagarDepartamentoR :: DepartamentoId -> Handler Html
+postApagarDepartamentoR deptoid = do 
+    runDB $ delete deptoid
+    redirect DepartamentosR
