d42a76edd92e589a1639881ddc57bd42487b85d7
Hugo Pedro <hugopedro098@gmail.com>
2018-12-08 16:13:35 +0000

    Atualizacao rotas

--- src/Handler/Dependente.hs
+++ src/Handler/Dependente.hs
@@ -10,3 +10,56 @@ import Import
 import Text.Lucius
 import Text.Julius
 import Database.Persist.Sql
+
+formDependente :: FormInput Handler Dependente
+formDependente = pure Dependente
+    <*> ireq textField "nome"
+    <*> ireq textField "rg"
+    <*> ireq dayField "dtnasc"
+    <*> ireq selectField (opt)
+
+getDependentesR :: Handler Html   
+getDependentesR = do 
+    dependentes <- runDB $ selectList [] [Asc DependenteId]
+    defaultLayout $ do 
+        $(whamletFile "templates/dependentes.hamlet")
+        toWidget[julius|
+            [].slice.call(document.getElementsByClassName("modal-trigger")).forEach(function(x){
+                x.addEventListener("click",function(){
+                    document.forms["frmEdicao"].setAttribute("action", this.dataset.edicao);
+                    document.forms["frmEdicao"].elements["nome"].setAttribute("value", this.dataset.depenome);
+                    document.forms["frmEdicao"].elements["rg"].setAttribute("value", this.dataset.deperg);
+                    document.forms["frmEdicao"].elements["dtnasc"].setAttribute("value", this.dataset.depedtnasc);
+                });
+            });
+        |]
+        toWidget[julius|
+            document.addEventListener('DOMContentLoaded', function() {
+                var elems = document.querySelectorAll('.modal');
+                var instances = M.Modal.init(elems,"");
+            });
+        |]
+        
+        
+postSalvarDependenteR :: Handler Html
+postSalvarDependenteR = do 
+    res <- runInputPostResult formDependente
+    case res of 
+        FormSuccess dependente -> do 
+            depeid <- runDB $ insert dependente
+            redirect DependentesR
+        _ -> redirect MenuR
+
+postApagarDependenteR :: DependenteId -> Handler Html
+postApagarDependenteR depeid = do 
+    runDB $ delete depeid
+    redirect DependentesR
+
+postEditarDependenteR :: DependenteId -> Handler Html
+postEditarDependenteR depeid = do
+    res <- runInputPostResult formDependente
+    case res of 
+        FormSuccess dependente -> do 
+            depeid <- runDB $ replace depeid dependente
+            redirect DependentesR
+        _ -> redirect MenuR
