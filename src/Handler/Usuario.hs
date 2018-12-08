ed4a92c6c5b4b702b09e06443547153898c03028
Paulo Henrique Marchotto <phomarchotto@hotmail.com>
2018-12-08 21:04:09 +0000

    Cadastro de Usuário

--- src/Handler/Usuario.hs
+++ src/Handler/Usuario.hs
@@ -10,3 +10,31 @@ import Import
 import Text.Lucius
 import Text.Julius
 import Database.Persist.Sql
+
+formUsuario :: FormInput Handler Usuario
+formUsuario = pure Usuario
+    <*> ireq textField "nome"
+    <*> ireq emailField "email"
+    <*> ireq passwordField "senha"
+    
+    
+getUsuariosR :: Handler Html   
+getUsuariosR = do 
+    usuarios <- runDB $ selectList [] [Asc UsuarioId]
+    defaultLayout $ do 
+        $(whamletFile "templates/usuario.hamlet")
+
+
+postSalvarUsuarioR :: Handler Html
+postSalvarUsuarioR = do 
+    res <- runInputPostResult formUsuario
+    case res of 
+        FormSuccess usuario -> do 
+            usuid <- runDB $ insert usuario
+            redirect UsuariosR
+        FormFailure erro -> do 
+            setMessage [shamlet|
+                        <h1>
+                            #{show erro}
+                    |]
+            redirect UsuariosR
